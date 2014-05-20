6851334f66cd2509920dcbe177809bbc83b18a89
DaveBueds <davebueds@MacBook-Pro-van-David.local>
2014-05-20 10:25:11 +0200

    Dave first commit locaties
    LocationsViewController toegevoegd

---
+++ ProjectMobile/ProjectMobile/LocationViewController.m
@@ -0,0 +1,13 @@
+//
+//  LocationViewController.m
+//  ProjectMobile
+//
+//  Created by David Bueds on 20/05/14.
+//  Copyright (c) 2014 johan. All rights reserved.
+//
+
+#import "LocationViewController.h"
+
+@implementation LocationViewController
+
+@end
