f424c4cf71c3245dfa8ef6d745a9e779f544e488
smq1991 <michael.smets1@student.ehb.be>
2014-05-20 10:19:49 +0200

    PersonenViewController toegevoegd
    Aanmaken personenviewcontroller

---
+++ ProjectMobile/ProjectMobile/PersonenViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonenViewController.h
+//  ProjectMobile
+//
+//  Created by Michael on 20/05/14.
+//  Copyright (c) 2014 johan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonenViewController : UIViewController
+
+@end
