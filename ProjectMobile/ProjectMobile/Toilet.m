5d7a15f213c14dfee2e992b4c67d819e3fdf4ff0
DaveBueds <davebueds@MacBook-Pro-van-David.local>
2014-05-21 11:56:05 +0200

    Json binnenhalen in objecten
    Json toiletten in mutablearray plaatsen

---
+++ ProjectMobile/ProjectMobile/Toilet.m
@@ -0,0 +1,14 @@
+//
+//  Toilet.m
+//  ProjectMobile
+//
+//  Created by David Bueds on 21/05/14.
+//  Copyright (c) 2014 johan. All rights reserved.
+//
+
+#import "Toilet.h"
+#import "JSONModelLib.h"
+
+@implementation Toilet
+
+@end
