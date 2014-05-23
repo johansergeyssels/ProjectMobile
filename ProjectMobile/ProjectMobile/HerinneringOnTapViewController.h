b5876242ec7f0910eb60c0ca63cbc4c3929641f3
AnneliesKoekoekx <annelies.koekoekx@DT-VM-OSX22.ehb.local>
2014-05-23 03:38:59 -0700

    Herinneringen On Tap toegevoegd

---
+++ ProjectMobile/ProjectMobile/HerinneringOnTapViewController.h
@@ -0,0 +1,13 @@
+//
+//  HerinneringOnTapViewController.h
+//  ProjectMobile
+//
+//  Created by KOEKOEKX Annelies (s) on 23/05/14.
+//  Copyright (c) 2014 johan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HerinneringOnTapViewController : UIViewController
+
+@end
---
+++ ProjectMobile/ProjectMobile/HerinneringOnTapViewController.m
@@ -0,0 +1,39 @@
+//
+//  HerinneringOnTapViewController.m
+//  ProjectMobile
+//
+//  Created by KOEKOEKX Annelies (s) on 23/05/14.
+//  Copyright (c) 2014 johan. All rights reserved.
+//
+
+#import "HerinneringOnTapViewController.h"
+
+@interface HerinneringOnTapViewController ()
+
+@end
+
+@implementation HerinneringOnTapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
